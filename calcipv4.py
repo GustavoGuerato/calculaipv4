1f93824505ebe3f2bfaf61f2eaf8dcc0d2780e7c
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-05 17:13:46 -0300

    add to the github

---
+++ calcipv4.py
@@ -0,0 +1,66 @@
+import re
+
+
+class CalcIpv4:
+    def __init__(self, ip, mascara=None, prefixo=None):
+        self.ip = ip
+        self.mascara = mascara
+        self.prefixo = prefixo
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @property
+    def mascara(self):
+        return self._mascara
+
+    @property
+    def prefixo(self):
+        return self._prefixo
+
+    @ip.setter
+    def ip(self, valor):
+        if not self._valida_ip(valor):
+            raise ValueError('IP Invalido')
+        self._ip = valor
+
+    @mascara.setter
+    def mascara(self, valor):
+        if not valor:
+            return
+        if not self._valida_ip(valor):
+            raise ValueError('Mascara Invalida')
+        self._mascara = valor
+        self._mascara_bin = self._ip_to_bin(valor)
+        print(self._mascara_bin)
+
+    @prefixo.setter
+    def prefixo(self, valor):
+        if not valor:
+            return
+
+        if not isinstance(valor, int):
+            raise TypeError('Prefixo precisa ser inteiro')
+
+        if valor > 32:
+            raise TypeError('Precisa ter 32bits')
+
+        self._prefixo = valor
+        self._mascara_bin = (valor * '1').ljust(32, '0')
+
+
+    @staticmethod
+    def _valida_ip(ip):
+        regexp = re.compile(
+            r'^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$'
+        )
+
+        if regexp.search(ip):
+            return True
+
+    @staticmethod
+    def _ip_to_bin(ip):
+        blocos = ip.split('.')
+        blocos_bin = [bin(int(x))[2:].zfill(8) for x in blocos]
+        return "".join(blocos_bin)
