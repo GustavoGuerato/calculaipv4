8db30328cb78c43b26e44f52b8c26d6e903f950d
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-06 18:35:21 -0300

    finishing the project

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from calcipv4 import CalcIpv4
+from calcipv4 import CalcIPv4
 
-calc_ipv4 = CalcIpv4(ip='192.168.0.1', mascara='255.255.255.0')
+calc_ipv4 = CalcIPv4(ip='192.168.0.1', mascara='255.255.255.0')
